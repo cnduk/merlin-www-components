33fc7761404dfbb604b36cc7c29f9675d2fcd9d5
danelliottpalmer <danelliottpalmer@gmail.com>
2017-02-15 11:34:25 +0000

    feat(merlin-sass-custom-importer): add component

---
+++ packages/merlin-sass-custom-importer/lib/index.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const path = require('path');
+const chalk = require('chalk');
+
+const COMPONENTS_DIR = path.normalize('node_modules');
+const LOGGER = {
+    'enabled': true,
+    'log': function(mode, ...args){
+        if(!this.enabled) return;
+        this[mode](...args);
+    },
+    'SKIP': function(...args){
+        console.log(chalk.underline.red(...args));
+    },
+    'LOAD': function(...args){
+        console.log(chalk.underline.green(...args));
+    }
+};
+const WIREFRAME_KEY = {
+    key: ':wireframe',
+    length: 10,
+    file: 'sass/wireframe/wireframe',
+    contains: function(url){
+        return url.endsWith(this.key);
+    },
+    resolve: function(url){
+        const baseUrl = url.substr(0, url.length - this.length);
+        return path.resolve(baseUrl, this.file);
+    }
+};
+const THEME_KEY = {
+    key: ':theme',
+    length: 6,
+    contains: function(url){
+        return url.endsWith(this.key);
+    },
+    resolve: function(url, theme){
+        const baseUrl = url.substr(0, url.length - this.length);
+        return path.resolve(baseUrl, `sass/${theme}/${theme}.scss`);
+    }
+};
+
+module.exports = function(merlinConfig={}, scopeName=null){
+
+    const SESSION = new Set();
+    SESSION.SKIPPED_ITEMS = 0;
+    SESSION.LOADED_ITEMS = 0;
+    SESSION.reset = function sessionReset(){
+        SESSION.clear();
+        SESSION.SKIPPED_ITEMS = 0;
+        SESSION.LOADED_ITEMS = 0;
+    };
+
+    const scope = scopeName ? `(${scopeName})` : '';
+
+    function importer(url, previous, done){
+
+        let sassUrl = null;
+
+        // Check if path begins with @, update url to be component based
+        if(url.startsWith('@')){
+
+            let customUrl = path.resolve(COMPONENTS_DIR, url);
+
+            // Check if we're using our keywords - theme, wireframe. If so, resolve
+            // the sass location
+            if(WIREFRAME_KEY.contains(customUrl)){
+                sassUrl = WIREFRAME_KEY.resolve(customUrl);
+            } else if(THEME_KEY.contains(customUrl)){
+                sassUrl = THEME_KEY.resolve(customUrl, merlinConfig.currentTheme);
+            } else {
+                sassUrl = customUrl;
+            }
+
+        // Resolve url normally, relatively
+        } else {
+            let urlParts = path.normalize(previous).split(path.sep);
+            urlParts.pop();
+            sassUrl = path.resolve(urlParts.join(path.sep), url);
+        }
+
+        correctSassPartials(sassUrl);
+
+        if(SESSION.has(sassUrl)){
+            LOGGER.log('SKIP', `SASS${scope}::Skipping ${sassUrl}`);
+            SESSION.SKIPPED_ITEMS++;
+            return done({ contents: '' });
+        }
+
+        LOGGER.log('LOAD', `SASS${scope}::Loading ${sassUrl}`);
+        SESSION.LOADED_ITEMS++;
+        SESSION.add(sassUrl);
+        return done({ file: sassUrl });
+    }
+
+    importer.SESSION = SESSION;
+
+    return importer;
+
+};
+module.exports.LOGGER = LOGGER;
+
+function correctSassPartials(url){
+    const urlPieces = url.split(path.sep);
+    let filename = urlPieces.pop();
+    if(filename.startsWith('_')){
+        filename = filename.slice(1);
+    }
+    return path.resolve(urlPieces.join(path.sep), filename);
+}
---
+++ packages/merlin-sass-custom-importer/readme.md
@@ -0,0 +1,9 @@
+# merlin-sass-custom-importer
+
+> Custom sass importer used for node-sass
+
+## Install
+
+```bash
+npm i -S @cnbritain/merlin-sass-custom-importer
+```
