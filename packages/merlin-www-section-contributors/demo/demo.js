996109c8dc77033f26dd559e8667f5697596dfc5
danelliottpalmer <danelliottpalmer@gmail.com>
2017-02-16 16:02:15 +0000

    feat(merlin-www-section-contributors): add component

---
+++ packages/merlin-www-section-contributors/demo/demo.js
@@ -0,0 +1,2 @@
+import demo from '../js/index';
+demo.init();
---
+++ packages/merlin-www-section-contributors/js/index.js
@@ -0,0 +1,7 @@
+import cardList from '@cnbritain/merlin-www-section-card-list';
+
+export default {
+    "init": function(){
+        cardList.init();
+    }
+};
