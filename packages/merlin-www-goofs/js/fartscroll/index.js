7eee535935c0bac2fe02fad8ba11393316402d67
danelliottpalmer <danelliottpalmer@gmail.com>
2017-06-01 16:45:56 +0100

    goof(left some silly code):

--- packages/merlin-www-goofs/js/fartscroll/index.js
+++ packages/merlin-www-goofs/js/fartscroll/index.js
@@ -3,8 +3,6 @@
 
 import { loadScript } from '@cnbritain/merlin-www-js-utils/js/functions';
 
-var SCRIPT_URL = ;
-
 export default function startthefarts(){
     loadScript("http://code.onion.com/fartscroll.js").then(function(){
         window.fartscroll();
