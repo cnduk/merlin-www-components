2aa30b8b5877adabcf52f0169fd15d862ef581cc
danelliottpalmer <danelliottpalmer@gmail.com>
2017-06-21 12:38:18 +0100

    fix(merlin-www-build-tools): append .min to minified css files

--- packages/merlin-www-build-tools/js/tasks/sass.js
+++ packages/merlin-www-build-tools/js/tasks/sass.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const path = require('path');
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 const SASS_IMPORTER = require('@cnbritain/merlin-sass-custom-importer');
 
 const utils = require('../utils');
@@ -11,8 +14,12 @@ const ENV = utils.getEnvironment();
 
 module.exports = function taskSassExport(taskConfig, browserSync){
     return function taskSass(){
-        let outputStyle = 'compressed';
-        if(ENV.isDev) outputStyle = 'expanded';
+        let outputStyle = 'expanded';
+        let renameConfig = {};
+        if(!ENV.isDev){
+            outputStyle = 'compressed';
+            renameConfig = { suffix: '.min' };
+        }
 
         const sassConfig = {
             importer: SASS_IMPORTER(taskConfig.merlin, taskConfig.merlin.name),
@@ -22,6 +29,7 @@ module.exports = function taskSassExport(taskConfig, browserSync){
         return gulp.src(taskConfig.sass.src)
             .pipe(sourcemaps.init())
             .pipe(sass(sassConfig).on('error', sass.logError))
+            .pipe(rename(renameConfig))
             // I have a feeling I'm going to need to check this
             .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest(taskConfig.sass.dest))
