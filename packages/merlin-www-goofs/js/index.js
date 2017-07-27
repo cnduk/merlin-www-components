d3d7fb343aeb51fd2f61a8e53325758a79c6ba69
danelliottpalmer <danelliottpalmer@gmail.com>
2017-07-27 17:11:27 +0100

    feat(merlin-www-goofs): add turn down for what

--- packages/merlin-www-goofs/js/index.js
+++ packages/merlin-www-goofs/js/index.js
@@ -2,3 +2,4 @@
 
 export { default as fartscroll } from './fartscroll';
 export { default as cookieflakes } from './cookieflakes';
+export { default as turnitdown } from './tdfw';
---
+++ packages/merlin-www-goofs/js/tdfw/index.js
@@ -0,0 +1,8 @@
+'use strict';
+/* globals fartscroll */
+
+import { loadScript } from '@cnbritain/merlin-www-js-utils/js/functions';
+
+export default function turnitdown(){
+    loadScript("https://nthitz.github.io/turndownforwhatjs/tdfw.js");
+}
