61d63c265aae3563d32dfb0f03a926389734ec74
Sam Tiffin <sam@samtiffin.com>
2017-09-07 12:43:57 +0100

    fix(timed-content): Update timed-content js to fix max-timeout issue

--- packages/merlin-www-bbcode/js/index.js
+++ packages/merlin-www-bbcode/js/index.js
@@ -9,6 +9,7 @@ import {
     removeClass
 } from '@cnbritain/merlin-www-js-utils/js/functions';
 
+
 var CLS_TIMED_CONTENT = 'bb-timed-content';
 var CLS_TIMED_CONTENT_PENDING = 'bb-timed-content--pending';
 var CLS_TIMED_CONTENT_ACTIVE = 'bb-timed-content--active';
@@ -19,9 +20,21 @@ function TimedContent() {
     EventEmitter.call(this, {wildcard: true});
 
     this.timedContentBlocks = toArray(document.querySelectorAll('.' + CLS_TIMED_CONTENT));
-
+    // We need to set a max timeout value as any number bigger than maxint will cause the timeout
+    // to fire immediately
+    this.maxTimeout = 86400 * 5; // 5 days seems plenty?
     this._init();
 }
+
+TimedContent._log = function() {
+    var DEBUG = false;
+
+    if (DEBUG) {
+        return console.log.apply(null, arguments);
+    }
+
+    return;
+};
 
 TimedContent.prototype = inherit(EventEmitter.prototype, {
     'constructor': TimedContent,
@@ -34,41 +47,65 @@ TimedContent.prototype = inherit(EventEmitter.prototype, {
         }
     },
 
+    '_parseDate': function(dateString) {
+        var parts = dateString.split(' ');
+        var date = parts[0];
+        var time = parts[1];
+
+        var dateParts = date.split('-');
+        var timeParts = time.substr(0, time.indexOf('+')).split(':');
+
+        return new Date(dateParts[0], dateParts[1] - 1, dateParts[2], timeParts[0], timeParts[1], timeParts[2]);
+    },
+
     'setTimers': function(el) {
-        if el.hasClass(CLS_TIMED_CONTENT_PREVIEW) { return; }
+        TimedContent._log("Setting timers for %o", el);
+        if (hasClass(el, CLS_TIMED_CONTENT_PREVIEW)) { TimedContent._log("- Preview mode, nothing to do here."); return; }
 
         var state = el.getAttribute('data-state');
 
-        if (!state) { return; }
+        if (!state) { TimedContent._log("- No state found."); return; }
+
+        TimedContent._log("- Found state: '%s'", state);
 
         var activeFrom = el.getAttribute('data-active-from');
         var activeTo = el.getAttribute('data-active-to');
 
+        TimedContent._log("- Active From: [%s] Active To: [%s]", activeFrom, activeTo);
+
         if (!activeFrom && !activeTo) { return; }
 
-        var activeFromDate = activeFrom ? new Date(activeFrom) : false;
-        var activeToDate = activeTo ? new Date(activeTo) : false;
+        var activeFromDate = activeFrom ? this._parseDate(activeFrom) : false;
+        var activeToDate = activeTo ? this._parseDate(activeTo) : false;
 
         var now = new Date();
 
+        TimedContent._log("- Now: [%s] Active From: [%s] Active To: [%s]", now, activeFromDate, activeToDate);
+
         switch (state) {
             case 'pending':
-                if (activeFromDate && now < activeFromDate) {
+                TimedContent._log("- Has PENDNG state.");
+                if (activeFromDate && now < activeFromDate && (activeFromDate - now) < this.maxTimeout) {
+                    TimedContent._log("- Will set active in %ds", (activeFromDate - now)/1000);
                     window.setTimeout(this.setActive, activeFromDate - now, el);
                 }
 
-                if (activeToDate && now < activeToDate) {
+                if (activeToDate && now < activeToDate  && (activeToDate - now) < this.maxTimeout) {
+                    TimedContent._log("- Will set expired in %ds", (activeToDate - now)/1000);
                     window.setTimeout(this.setExpired, activeToDate - now, el);
                 }
             break;
 
             case 'active':
-                if (activeToDate && now < activeToDate) {
+                TimedContent._log("- Has ACTIVE state.");
+                if (activeToDate && now < activeToDate && (activeToDate - now) < this.maxTimeout) {
+                    TimedContent._log("- Will set expired in %ds", (activeToDate - now)/1000);
                     window.setTimeout(this.setExpired, activeToDate - now, el);
                 }
             break;
 
             case 'expired':
+                TimedContent._log("- Has EXPIRED state.");
                 this.setExpired(el);
             break;
 
@@ -79,13 +116,14 @@ TimedContent.prototype = inherit(EventEmitter.prototype, {
     },
 
     'setActive': function(el) {
+        TimedContent._log("Setting %s to state [ACTIVE]", el);
         el.setAttribute('data-state', 'active');
-        // in theory we'll never go from expired to active...
-        removeClass(el, CLS_TIMED_CONTENT_PENDING);
+        removeClass(el, CLS_TIMED_CONTENT_PENDING); // in theory we'll never go from expired to active...
         addClass(el, CLS_TIMED_CONTENT_ACTIVE);
     },
 
     'setExpired': function(el) {
+        TimedContent._log("Setting %s to state [EXPIRED]", el);
         el.remove();
     }
 });
