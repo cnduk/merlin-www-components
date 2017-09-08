b619a19334c2f75a6d9bdc2058eac64b9b399c0b
danelliottpalmer <danelliottpalmer@gmail.com>
2017-09-08 11:33:38 +0100

    fix(merlin-www-figure): correct full-screen sass and js for touch devices

--- packages/merlin-www-figure/js/magnifier/constants.js
+++ packages/merlin-www-figure/js/magnifier/constants.js
@@ -7,7 +7,7 @@ export var JS_MAGNIFIER_BUTTON_ZOOM_IN = 'js-c-figure-magnify-zoom-in';
 export var JS_MAGNIFIER_BUTTON_ZOOM_OUT = 'js-c-figure-magnify-zoom-out';
 
 export var CLS_MAGNIFIER = 'c-figure__magnifier';
-export var CLS_MAGNIFIER_FULLSCREEN = 'c-figure__magnifier--fullscreen';
+export var CLS_MAGNIFIER_FULLSCREEN = 'c-figure__magnifier--full-screen';
 export var CLS_MAGNIFIER_IMAGE = 'c-figure__magnifier-image';
 export var CLS_HAS_MAGNIFIER = 'has-magnifier';
 export var CLS_IS_HIDDEN = 'is-hidden';
