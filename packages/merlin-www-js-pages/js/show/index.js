e84bc57694347a42df881f05ff1a6e3158a04485
danelliottpalmer <danelliottpalmer@gmail.com>
2017-10-03 15:37:34 +0100

    fix(merlin-www-js-page): correct missing show index js

--- packages/merlin-www-js-pages/js/show/index.js
+++ packages/merlin-www-js-pages/js/show/index.js
@@ -1,9 +1,16 @@
 'use strict';
 
+import ShowFinder from '@cnbritain/merlin-www-show-finder';
+import ShowSummary from '@cnbritain/merlin-www-show-summary';
+
 import { default as initAds } from './ads';
 import { default as initInfinite } from './infinite';
+import { default as initSticky, createStickGroup } from '../tagpage/sticky';
 
 export default function init(){
     initAds();
+    ShowFinder.init();
+    ShowSummary.init();
+    initSticky();
     initInfinite();
 }
