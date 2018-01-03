8fb812e0c4c3ecc3da9386c5628dac9d80486953
danelliottpalmer <danelliottpalmer@gmail.com>
2018-01-03 16:56:56 +0000

    fix(merlin-www-goofs): eslint the js

--- packages/merlin-www-goofs/js/raptor/index.js
+++ packages/merlin-www-goofs/js/raptor/index.js
@@ -1,48 +1,48 @@
-"use strict";
+'use strict';
 
 /**
  * In memory of https://github.com/acrogenesis/raptorize
  * I refuse to load jquery for it.
  */
 
-var AUDIO_ID = "elRaptorShriek";
-var RAPTOR_ID = "imgRaptor";
+var AUDIO_ID = 'elRaptorShriek';
+var RAPTOR_ID = 'imgRaptor';
 var KEYS = [38,38,40,40,37,39,37,39,66,65];
-var INLINE_CSS = "@-webkit-keyframes raptor{0%{bottom:-500px;}70%{bottom:0;right:0%;}100%{bottom:0;right:110%;}}@keyframes raptor{0%{bottom:-500px;}70%{bottom:0;right:0%;}100%{bottom:0;right:110%;}}.do-the-raptor{-webkit-animation-duration:3s;-ms-animation-duration:3s;-moz-animation-duration:3s;-o-animation-duration:3s;animation-duration:3s;-webkit-animation-name:raptor;-ms-animation-name:raptor;-moz-animation-name:raptor;-o-animation-name:raptor;animation-name:raptor;}";
+var INLINE_CSS = '@-webkit-keyframes raptor{0%{bottom:-500px;}70%{bottom:0;right:0%;}100%{bottom:0;right:110%;}}@keyframes raptor{0%{bottom:-500px;}70%{bottom:0;right:0%;}100%{bottom:0;right:110%;}}.do-the-raptor{-webkit-animation-duration:3s;-ms-animation-duration:3s;-moz-animation-duration:3s;-o-animation-duration:3s;animation-duration:3s;-webkit-animation-name:raptor;-ms-animation-name:raptor;-moz-animation-name:raptor;-o-animation-name:raptor;animation-name:raptor;}';
 
 var hasElements = false;
 var hasListeners = false;
 var keysLeft = null;
 
 function addStyles(){
-    var styles = createTag("style", {"type": "text/css"});
+    var styles = createTag('style', {'type': 'text/css'});
     if (styles.styleSheet){
-      styles.styleSheet.cssText = INLINE_CSS;
+        styles.styleSheet.cssText = INLINE_CSS;
     } else {
-      styles.appendChild( document.createTextNode( INLINE_CSS ) );
+        styles.appendChild( document.createTextNode( INLINE_CSS ) );
     }
     document.head.appendChild( styles );
 }
 
 function createRaptorElement(){
-    var raptor = createTag( "img", {
-        "id": RAPTOR_ID,
-        "src": "../static/img/raptor.png",
-        "style": "position: fixed; bottom: -500px; right: 0%; z-index: 10"
+    var raptor = createTag( 'img', {
+        'id': RAPTOR_ID,
+        'src': '../static/img/raptor.png',
+        'style': 'position: fixed; bottom: -500px; right: 0%; z-index: 10'
     });
     return raptor;
 }
 
 function createSoundElement(){
-    var audioEl = createTag( "audio", {
-        "id": AUDIO_ID,
-        "preload": "auto"
+    var audioEl = createTag( 'audio', {
+        'id': AUDIO_ID,
+        'preload': 'auto'
     });
-    var mp3Source = createTag( "source", {
-        "src": "../static/sounds/raptor-sound.mp3"
+    var mp3Source = createTag( 'source', {
+        'src': '../static/sounds/raptor-sound.mp3'
     });
-    var oggSource = createTag( "source", {
-        "src": "../static/sounds/raptor-sound.ogg"
+    var oggSource = createTag( 'source', {
+        'src': '../static/sounds/raptor-sound.ogg'
     });
     audioEl.appendChild( mp3Source );
     audioEl.appendChild( oggSource );
@@ -66,16 +66,16 @@ function destroy(){
     KEYS.length = 0;
 }
 
-function fireAnalytics(){
-    GATracker.SendAll( GATracker.SEND_HITTYPES.EVENT, {
-        'eventCategory': 'Secrets',
-        'eventAction': 'Raptor found'
-    });
-}
+// function fireAnalytics(){
+//     GATracker.SendAll( GATracker.SEND_HITTYPES.EVENT, {
+//         'eventCategory': 'Secrets',
+//         'eventAction': 'Raptor found'
+//     });
+// }
 
 function init(){
     reset();
-    window.addEventListener( "keydown", onKeyDown );
+    window.addEventListener( 'keydown', onKeyDown );
     hasListeners = true;
 }
 
@@ -101,8 +101,8 @@ function raptor(){
     document.body.appendChild( sound );
     document.body.appendChild( rap );
 
-    var forceRedraw = rap.offsetWidth;
-    rap.className = "do-the-raptor";
+    var forceRedraw = rap.offsetWidth; // eslint-disable-line no-unused-vars
+    rap.className = 'do-the-raptor';
 
     tryCatch(function(){ sound.play(); });
     sound = null;
@@ -126,7 +126,7 @@ function removeElements(){
 
 function removeListeners(){
     if( !hasListeners ) return;
-    window.removeEventListener( "keydown", onKeyDown );
+    window.removeEventListener( 'keydown', onKeyDown );
 }
 
 function reset(){
