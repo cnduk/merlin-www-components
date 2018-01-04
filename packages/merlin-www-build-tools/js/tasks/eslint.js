a77494cd6b3521f8c00443bbdf59e00cba5048fd
danelliottpalmer <danelliottpalmer@gmail.com>
2018-01-04 15:53:22 +0000

    fix(merlin-www-build-tools): move .eslintrc into eslint task

--- packages/merlin-www-build-tools/js/tasks/eslint.js
+++ packages/merlin-www-build-tools/js/tasks/eslint.js
@@ -6,7 +6,41 @@ const eslint = require('gulp-eslint');
 module.exports = function taskEsLintExport(taskConfig, browserSync){
     return function taskEsLint(done){
         return gulp.src(taskConfig.eslint.src)
-            .pipe(eslint({useEslintrc: true}))
+            .pipe(eslint({
+                "env": {
+                    "browser": true,
+                    "commonjs": true,
+                    "es6": true
+                },
+                "extends": "eslint:recommended",
+                "parserOptions": {
+                    "sourceType": "module"
+                },
+                "rules": {
+                    "indent": [
+                        "error",
+                        4
+                    ],
+                    "linebreak-style": [
+                        "error",
+                        "unix"
+                    ],
+                    "quotes": [
+                        "error",
+                        "single"
+                    ],
+                    "semi": [
+                        "error",
+                        "always"
+                    ],
+                    "no-console": [
+                        "error",
+                        {
+                            "allow": ["warn", "error"]
+                        }
+                    ]
+                }
+            }))
             .pipe(eslint.failAfterError())
             .pipe(eslint.format('stylish'));
     };
