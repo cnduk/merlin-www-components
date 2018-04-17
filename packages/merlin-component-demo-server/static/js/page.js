cacc7caa877a6e2d8d2c7286ead97e5ed43ea4b7
danelliottpalmer <danelliottpalmer@gmail.com>
2018-04-17 15:23:21 +0100

    fix(merlin-component-demo-server): add device width meta to rendered page

--- packages/merlin-component-demo-server/static/js/page.js
+++ packages/merlin-component-demo-server/static/js/page.js
@@ -130,6 +130,7 @@
             <html>
                 <head>
                     <title></title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
                 </head>
                 <body>
                     ${html}
