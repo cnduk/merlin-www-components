f7750abde016bdd2d79ff920c2c5e09557bbfe30
Stefan Piazza <stefanpiazza@gmail.com>
2018-06-21 12:12:35 +0100

    style(melrin-www-main-navigation): glamour styles added

--- packages/merlin-www-main-navigation/demo/demo.js
+++ packages/merlin-www-main-navigation/demo/demo.js
@@ -21,4 +21,32 @@ for (var i = 0; i < 10; i++) {
     document.body.appendChild(bodyCopy);
 }
 
-
+// Typekit font loading
+(function(d) {
+    var config = {
+            kitId: 'lfx7aae',
+            scriptTimeout: 3000,
+            async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+            h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+    h.className += " wf-loading";
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+    tk.async = true;
+    tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || a && a != "complete" && a != "loaded") return;
+        f = true;
+        clearTimeout(t);
+        try {
+            Typekit.load(config)
+        } catch (e) {}
+    };
+    s.parentNode.insertBefore(tk, s)
+})(document);
