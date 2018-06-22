af9dabdef1bbde7f04bed4b87b71398a08b78ab3
Stefan Piazza <stefanpiazza@gmail.com>
2018-06-22 15:24:19 +0100

    feat(merlin-www-main-navigation): tatler styles added

--- packages/merlin-www-main-navigation/demo/demo.js
+++ packages/merlin-www-main-navigation/demo/demo.js
@@ -108,3 +108,32 @@ for (var i = 0; i < 10; i++) {
     };
     s.parentNode.insertBefore(tk, s)
 })(document);
+
+(function(d) {
+    var config = {
+            kitId: 'lcd0ukg',
+            scriptTimeout: 3000,
+            async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+            h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+    h.className += " wf-loading";
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+    tk.async = true;
+    tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || a && a != "complete" && a != "loaded") return;
+        f = true;
+        clearTimeout(t);
+        try {
+            Typekit.load(config)
+        } catch (e) {}
+    };
+    s.parentNode.insertBefore(tk, s)
+})(document);
