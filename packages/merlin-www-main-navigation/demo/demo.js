2649280254ad1e7abc0a7e69f423fdf12397a85e
Stefan Piazza <stefanpiazza@gmail.com>
2018-06-25 13:59:03 +0100

    style(merlin-www-main-navigation): gq styles added

--- packages/merlin-www-main-navigation/demo/demo.js
+++ packages/merlin-www-main-navigation/demo/demo.js
@@ -137,3 +137,14 @@ for (var i = 0; i < 10; i++) {
     };
     s.parentNode.insertBefore(tk, s)
 })(document);
+
+(function() {
+    var link = document.createElement("link");
+
+    link.href = "http://www.gq-magazine.co.uk/static/fonts/gotham/581333/885C1C0CB36CDDA2B.css";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.media = "screen,print";
+
+    document.getElementsByTagName("head")[0].appendChild(link);
+})(document);
