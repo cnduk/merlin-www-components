9a70df2deb285a9ef7bd149c2ccc9b93292a6476
Stefan Piazza <stefanpiazza@gmail.com>
2018-08-03 13:04:00 +0100

    fix(merlin-www-main-navigation): create nav, not infobar

--- packages/merlin-www-main-navigation/js/index.js
+++ packages/merlin-www-main-navigation/js/index.js
@@ -3,7 +3,7 @@ import Nav from './nav';
 var NAV;
 
 if (document.querySelector('.js-c-nav')) {
-    NAV = new Infobar(document.querySelector('.js-c-nav'));
+    NAV = new Nav(document.querySelector('.js-c-nav'));
 }
 
 export default NAV;
