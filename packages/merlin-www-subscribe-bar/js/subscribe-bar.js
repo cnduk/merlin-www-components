4be3cb6b5394ec7ea8455a6ed5252eb80fa43d85
danelliottpalmer <danelliottpalmer@gmail.com>
2018-12-13 11:19:25 +0000

    feat(merlin-www-subscribe-bar): add page view limits
    [branch ch4825]

--- packages/merlin-www-subscribe-bar/js/subscribe-bar.js
+++ packages/merlin-www-subscribe-bar/js/subscribe-bar.js
@@ -4,30 +4,40 @@ import EventEmitter from 'eventemitter2';
 import {
     addEvent,
     addClass,
-    removeClass,
-    getCookie,
     inherit,
-    setCookie
+    removeClass,
+    removeEvent
 } from '@cnbritain/merlin-www-js-utils/js/functions';
 import NAV from '@cnbritain/merlin-www-main-navigation';
+import {VIEW_LIMIT} from './constants';
+import {loadConfig, saveConfig} from './utils';
 import * as events from './events';
 
 var IS_HIDDEN_CLS = 'is-hidden';
 var IS_FIXED_CLS = 'is-fixed';
 var IS_DISABLED_CLS = 'is-disabled';
 
-var COOKIE_PAGE_VIEW_DATE = 'cnd_subscribe_bar_pageview_date';
-var COOKIE_HASH = 'cnd_subscribe_bar_hash';
-var COOKIE_CONVERTED = 'cnd_subscribe_bar_converted';
-
 function SubscribeBar(el) {
     EventEmitter.call(this);
 
+    // Check we have an element
+    if(!el) throw new Error('Subscribe Bar Element Not Found');
     this.el = el;
 
-    if (!this.el) {
-        throw new Error('Subscribe Bar Element Not Found');
-    }
+    // Check we have a config
+    this.currentConfigHash = null;
+    this.parseConfig();
+
+    // Get elements
+    this.formEl = el.querySelector('.js-c-subscribe-bar__form');
+    this.emailEl = el.querySelector('.js-c-subscribe-bar__form-text');
+    this.contentEl = el.querySelector('.js-c-subscribe-bar__content');
+    this.statusEl = el.querySelector('.js-c-subscribe-bar__status');
+    this.spinnerEl = el.querySelector('.js-c-subscribe-bar__spinner');
+    this.successEl = el.querySelector('.js-c-subscribe-bar__success-message');
+    this.failureEl = el.querySelector('.js-c-subscribe-bar__failure-message');
+    this.closeButtonEls = el.querySelectorAll(
+        '.js-c-subscribe-bar__close-button');
 
     this.state = {
         isHidden: false,
@@ -35,40 +45,10 @@ function SubscribeBar(el) {
         isEnabled: false
     };
 
-    this.formEl = el.querySelector('.js-c-subscribe-bar__form');
-    this.emailEl = el.querySelector('.js-c-subscribe-bar__form-text');
-
-    this.contentEl = el.querySelector('.js-c-subscribe-bar__content');
-
-    this.statusEl = el.querySelector('.js-c-subscribe-bar__status');
-    this.spinnerEl = el.querySelector('.js-c-subscribe-bar__spinner');
-    this.successEl = el.querySelector('.js-c-subscribe-bar__success-message');
-    this.failureEl = el.querySelector('.js-c-subscribe-bar__failure-message');
-
-    this.closeButtonEls = el.querySelectorAll('.js-c-subscribe-bar__close-button');
-
-    var configEl = el.querySelector('.js-c-subscribe-bar-config');
-    if (!configEl) {
-        throw new Error('Subscribe Bar Config Not Found.');
-    }
-    this.config = JSON.parse(configEl.innerHTML);
-
-    this.previousHash = getCookie('cnd_subscribe_bar_hash');
-    this.currentHash = this.config['hash'];
-
-    addEvent(this.formEl, 'submit', this.onSubmit.bind(this));
-
-    for (var i = 0; i < this.closeButtonEls.length; i++) {
-        addEvent(this.closeButtonEls[i], 'click', this.disable.bind(this));
-    }
-
-    addEvent(this.emailEl, 'blur', function() {
-        if (this.checkValidity()) {
-            removeClass(this, 'has-error');
-        } else {
-            addClass(this, 'has-error');
-        }
-    });
+    this._submitListener = null;
+    this._disableListener = null;
+    this._blurListener = null;
+    this.bindListeners();
 
     if (NAV) {
         NAV.on('show', this.show.bind(this));
@@ -80,20 +60,66 @@ function SubscribeBar(el) {
 
 SubscribeBar.prototype = inherit(EventEmitter.prototype, {
 
+    bindListeners: function bindListeners(){
+        this._submitListener = this.onSubmit.bind(this);
+        addEvent(this.formEl, 'submit', this._submitListener);
+
+        this._disableListener = this.disable.bind(this);
+        for (var i = 0; i < this.closeButtonEls.length; i++) {
+            addEvent(this.closeButtonEls[i], 'click', this._disableListener);
+        }
+
+        this._blurListener = this.onBlur.bind(this);
+        addEvent(this.emailEl, 'blur', this._blurListener);
+    },
+
+    unbindListeners: function unbindListeners(){
+        removeEvent(this.formEl, 'submit', this._submitListener);
+        this._submitListener = null;
+
+        for (var i = 0; i < this.closeButtonEls.length; i++) {
+            removeEvent(this.closeButtonEls[i], 'click', this._disableListener);
+        }
+        this._disableListener = null;
+
+        removeEvent(this.emailEl, 'blur', this._blurListener);
+        this._blurListener = null;
+    },
+
+    parseConfig: function parseConfig(){
+        // Check we have a config
+        var configEl = this.el.querySelector('.js-c-subscribe-bar-config');
+        if (!configEl) throw new Error('Subscribe Bar Config Not Found.');
+        try {
+            this.config = JSON.parse(configEl.innerHTML);
+        } catch(err){
+            console.error('Error parsing subscribe bar config!');
+            throw err;
+        }
+
+        this.currentConfigHash = this.config.hash;
+    },
+
     init: function() {
+        var savedConfig = loadConfig();
 
-        if (this.previousHash !== this.currentHash) {
-            setCookie(COOKIE_HASH, this.currentHash);
-            setCookie(COOKIE_PAGE_VIEW_DATE, false);
-        }
+        // Check if the user has converted
+        if(savedConfig.converted){
+            this.disable();
 
-        var convertedCookie = getCookie(COOKIE_CONVERTED);
-        if (convertedCookie) {
-            return;
-        }
+        // Check if the config hash has changed
+        } else if(savedConfig.configHash !== this.currentConfigHash){
+            saveConfig(this.currentConfigHash, false, 1);
+            this.enable();
 
-        var viewDateCookie = getCookie(COOKIE_PAGE_VIEW_DATE);
-        if (viewDateCookie === null || viewDateCookie === 'false') {
+        // Check if the user has exceeded views
+        } else if(savedConfig.viewExceeded){
+            this.disable();
+
+        // User should see the message and bump view count
+        } else {
+            saveConfig(
+                this.currentConfigHash, false, savedConfig.viewCount + 1);
             this.enable();
         }
     },
@@ -149,7 +175,9 @@ SubscribeBar.prototype = inherit(EventEmitter.prototype, {
         addClass(this.el, IS_DISABLED_CLS);
         this.state.isEnabled = false;
 
-        setCookie(COOKIE_PAGE_VIEW_DATE, true, 30);
+        var savedConfig = loadConfig();
+        saveConfig(
+            savedConfig.configHash, savedConfig.converted, VIEW_LIMIT);
 
         this.emit('disable', events.disable(this));
     },
@@ -173,7 +201,9 @@ SubscribeBar.prototype = inherit(EventEmitter.prototype, {
 
                 if (xhr.status === 200) {
                     removeClass(this.successEl, IS_HIDDEN_CLS);
-                    setCookie(COOKIE_CONVERTED, true, 30);
+                    var savedConfig = loadConfig();
+                    saveConfig(
+                        savedConfig.configHash, true, savedConfig.viewCount);
                     this.emit('signup', events.signup(this));
                 } else {
                     removeClass(this.failureEl, IS_HIDDEN_CLS);
@@ -181,7 +211,16 @@ SubscribeBar.prototype = inherit(EventEmitter.prototype, {
             }
         }.bind(this);
         xhr.send('email=' + email);
+    },
+
+    onBlur: function(e){
+        if (e.target.checkValidity()) {
+            removeClass(e.target, 'has-error');
+        } else {
+            addClass(e.target, 'has-error');
+        }
     }
+
 });
 
 export default SubscribeBar;
---
+++ packages/merlin-www-subscribe-bar/js/utils.js
@@ -0,0 +1,39 @@
+
+import {getCookie, setCookie} from '@cnbritain/merlin-www-js-utils/js/functions';
+import {COOKIE_CONFIG_HASH, COOKIE_EXPIRES, VIEW_LIMIT} from './constants';
+
+export function saveConfig(configHash, converted, viewCount){
+    var config = {
+        configHash: configHash,
+        converted: !!converted,
+        viewCount: viewCount,
+        viewExceeded: viewCount >= VIEW_LIMIT
+    };
+    setCookie(COOKIE_CONFIG_HASH, JSON.stringify(config), COOKIE_EXPIRES);
+}
+
+export function loadConfig(){
+    var config = getCookie(COOKIE_CONFIG_HASH);
+    if(config){
+        try {
+            config = JSON.parse(config);
+        } catch(err){
+            console.error('Error parsing config!');
+            console.error(err);
+            config = {
+                configHash: null,
+                converted: false,
+                viewCount: 0,
+                viewExceeded: false
+            };
+        }
+    } else {
+        config = {
+            configHash: null,
+            converted: false,
+            viewCount: 0,
+            viewExceeded: false
+        };
+    }
+    return config;
+}
