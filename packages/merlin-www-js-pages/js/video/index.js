87270dffb8f6970fda694959ca0b0cd6d154cced
danelliottpalmer <danelliottpalmer@gmail.com>
2019-01-14 11:43:30 +0000

    fix(merlin-www-js-pages): fire pageview on video articles when focus changes

--- packages/merlin-www-js-pages/js/video/index.js
+++ packages/merlin-www-js-pages/js/video/index.js
@@ -4,27 +4,19 @@ import CONFIG_BRAND from '@cnbritain/merlin-www-common';
 import {
     getNamespaceKey
 } from '@cnbritain/merlin-www-js-utils/js/functions';
-import {
-    ArticleManager
-} from '@cnbritain/merlin-www-article';
-import {
-    default as initAds
-} from './ads';
-import {
-    default as initArticle
-} from './article';
-import {
-    default as initInfinite
-} from './infinite';
-import {
-    default as initSticky
-} from './sticky';
+import {ArticleManager} from '@cnbritain/merlin-www-article';
+import {default as initAds} from './ads';
+import {default as initArticle} from './article';
+import {default as initInfinite} from './infinite';
+import {default as initSticky} from './sticky';
+import {default as initAnalytics} from './analytics';
 
 export default function init() {
     initAds();
     initArticle();
     initInfinite();
     initSticky();
+    initAnalytics();
 
     // Global namespace stuffs
     // Don't just use the abbreviation in case something else in the page
