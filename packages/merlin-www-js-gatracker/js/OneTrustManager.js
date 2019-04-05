3aef3a90f545039c4d53e21a15176422cf3a3416
danelliottpalmer <danelliottpalmer@gmail.com>
2019-04-05 17:24:24 +0100

    feat(merlin-www-js-gatracker): onetrustmanager now handles different consent levels

--- packages/merlin-www-js-gatracker/js/OneTrustManager.js
+++ packages/merlin-www-js-gatracker/js/OneTrustManager.js
@@ -16,27 +16,34 @@ function OneTrustManager() {
     });
     this.ONETRUST_COOKIE = 'cnd_one_trust_consent1';
     this._loadingScript = false;
-    // -1: dialog still open so ignoring
-    // 0: dialog closed and rejected
-    // 1: dialog closed and consented
-    this.consent = -1;
+    this.isDialogClosed = false;
+    this.consentData = null;
+    this.consentedStrictlyCookies = false;
+    this.consentedPerformanceCookies = false;
+    this.consentedFunctionalCookies = false;
+    this.consentedTargetingCookies = false;
 }
 
 OneTrustManager.prototype = inherit(EventEmitter.prototype, {
+
     constructor: OneTrustManager,
-    getConsent: function getConsent() {
-        var cookieValue = getCookie(this.ONETRUST_COOKIE);
-        if (cookieValue !== null) {
-            return cookieValue;
-        } else {
-            return false;
+
+    _processConsentData: function _processConsentData(){
+        if(this.consentData){
+            this.consentedStrictlyCookies = /,1,/.test(this.consentData);
+            this.consentedPerformanceCookies = /,2,/.test(this.consentData);
+            this.consentedFunctionalCookies = /,3,/.test(this.consentData);
+            this.consentedTargetingCookies = /,4,/.test(this.consentData);
         }
+        this.isDialogClosed = !!getCookie('OptanonAlertBoxClosed');
     },
+
     setConsent: function setConsent(value) {
-        var beforeValue = this.consent;
+        var beforeValue = this.consentData;
 
         setCookie(this.ONETRUST_COOKIE, value);
-        this.consent = value;
+        this.consentData = value;
+        this._processConsentData();
 
         if(beforeValue !== this.consent){
             this.emit('change', createEventTemplate('change', this, {
@@ -44,6 +51,11 @@ OneTrustManager.prototype = inherit(EventEmitter.prototype, {
             }));
         }
     },
+
+    loadConsent: function loadConsent(){
+        this.setConsent(getCookie(this.ONETRUST_COOKIE));
+    },   
+
     init: function init(options) {
         if (!options.hasOwnProperty('script_url')) {
             throw new Error('Missing script_url value');
@@ -52,37 +64,18 @@ OneTrustManager.prototype = inherit(EventEmitter.prototype, {
         // Set the callback before trying to load the script
         window.OptanonWrapper = function() {
             if (window.OptanonActiveGroups) {
-                // Need to wrap this in a timeout as the dialog doesnt close before this
-                // bloody function.
                 setTimeout(function(){
-                    var consentToTargeting = /,4,/.test(window.OptanonActiveGroups);
-
-                    if(!Optanon.IsAlertBoxClosed()){
-                        this.setConsent(-1);
-                    } else {
-                        if (consentToTargeting) {
-                            this.setConsent(1);
-                        } else {
-                            this.setConsent(0);
-                        }
-                    }
+                    this.setConsent(window.OptanonActiveGroups);
+                    console.log(this);
                 }.bind(this), 100);
             }
         }.bind(this);
 
         this.loadOneTrustScript(options.script_url);
-
-        var oneTrustConsent = this.getConsent();
-        if (oneTrustConsent !== false) {
-            this.consent = parseInt(oneTrustConsent, 10);
-            return;
-        }
-
-        if (!hasCookiesEnabled) {
-            this.setConsent(0);
-            return;
-        }
+        this.loadConsent();
+        if(!hasCookiesEnabled) this.setConsent(',,');
     },
+
     loadOneTrustScript: function loadOneTrustScript(scriptUrl) {
         if (this._loadingScript) return;
 
@@ -96,6 +89,7 @@ OneTrustManager.prototype = inherit(EventEmitter.prototype, {
                 this._loadingScript = false;
             }.bind(this));
     }
+
 });
 
 export default new OneTrustManager();
