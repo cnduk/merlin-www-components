d402ed29b8e7aa06420f5e01751b85006e047f1d
danelliottpalmer <danelliottpalmer@gmail.com>
2019-12-21 17:24:59 +0000

    feat(merlin-www-js-gatracker): add SkimlinksManager

---
+++ packages/merlin-www-js-gatracker/js/SkimlinksManager.js
@@ -0,0 +1,36 @@
+import EventEmitter from 'eventemitter2';
+import {
+    inherit,
+    loadScript
+} from '@cnbritain/merlin-www-js-utils/js/functions';
+
+function SkimlinksManager() {
+    EventEmitter.call(this, {
+        wildcard: true
+    });
+    this._skimlinksId = null;
+    this._hasLoadedScript = false;
+}
+
+SkimlinksManager.prototype = inherit(EventEmitter.prototype, {
+    constructor: SkimlinksManager,
+
+    init: function init(skimlinksId) {
+        this._skimlinksId = skimlinksId;
+    },
+
+    loadScript: function loadScript() {
+        if (this._hasLoadedScript) return;
+        if (this._skimlinksId === null) {
+            throw new TypeError('Missing Skimlinks Id', this._skimlinksId);
+        }
+        this._hasLoadedScript = true;
+        loadScript(
+            'https://s.skimresources.com/js/' +
+                this._skimlinksId +
+                '.skimlinks.js'
+        );
+    }
+});
+
+export default new SkimlinksManager();
