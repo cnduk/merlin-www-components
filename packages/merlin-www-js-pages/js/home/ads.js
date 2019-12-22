4c810ad6ed4403c2ac806a1871412d33b57ec243
danelliottpalmer <danelliottpalmer@gmail.com>
2019-12-22 17:18:34 +0000

    style(merlin-www-js-pages): prettier

--- packages/merlin-www-js-pages/js/home/ads.js
+++ packages/merlin-www-js-pages/js/home/ads.js
@@ -4,31 +4,31 @@ import { AdManager } from '@cnbritain/merlin-www-ads';
 import OneTrustManager from '@cnbritain/merlin-www-js-gatracker/js/OneTrustManager';
 
 export default function init() {
-  function onChange() {
-    if (this.consentedPerformanceCookies) {
-      OneTrustManager.off('change', onChange);
-      AdManager.init();
-      AdManager.lazy();
+    function onChange() {
+        if (this.consentedPerformanceCookies) {
+            OneTrustManager.off('change', onChange);
+            AdManager.init();
+            AdManager.lazy();
+        }
     }
-  }
 
-  function onReady() {
-    if (this.consentedPerformanceCookies) {
-      AdManager.init();
-      AdManager.lazy();
-    } else {
-      OneTrustManager.on('change', onChange);
+    function onReady() {
+        if (this.consentedPerformanceCookies) {
+            AdManager.init();
+            AdManager.lazy();
+        } else {
+            OneTrustManager.on('change', onChange);
+        }
     }
-  }
 
-  if (OneTrustManager.ready) {
-    if (OneTrustManager.consentedPerformanceCookies) {
-      AdManager.init();
-      AdManager.lazy();
+    if (OneTrustManager.ready) {
+        if (OneTrustManager.consentedPerformanceCookies) {
+            AdManager.init();
+            AdManager.lazy();
+        } else {
+            OneTrustManager.on('change', onChange);
+        }
     } else {
-      OneTrustManager.on('change', onChange);
+        OneTrustManager.once('ready', onReady);
     }
-  } else {
-    OneTrustManager.once('ready', onReady);
-  }
 }
