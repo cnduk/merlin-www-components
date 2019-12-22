35529fcb2f69b6ffec8be893191dc839c579f8cb
danelliottpalmer <danelliottpalmer@gmail.com>
2019-12-22 13:56:32 +0000

    feat(merlin-www-js-gatracker): add ready event to OneTrustManager

--- packages/merlin-www-js-gatracker/js/OneTrustManager.js
+++ packages/merlin-www-js-gatracker/js/OneTrustManager.js
@@ -1,21 +1,20 @@
-import EventEmitter from "eventemitter2";
+import EventEmitter from 'eventemitter2';
 import {
     getCookie,
     inherit,
     loadScript,
     setCookie,
     createEventTemplate
-} from "@cnbritain/merlin-www-js-utils/js/functions";
-import { hasCookiesEnabled } from "@cnbritain/merlin-www-js-utils/js/detect";
+} from '@cnbritain/merlin-www-js-utils/js/functions';
+import { hasCookiesEnabled } from '@cnbritain/merlin-www-js-utils/js/detect';
 
-var ONETRUST_CONSENT_COOKIE = "OptanonConsent";
+var ONETRUST_CONSENT_COOKIE = 'OptanonConsent';
 
 var VERSION_1 = {
     loadConsent: function loadConsent() {
         this.setConsent(getCookie(this.ONETRUST_COOKIE));
     },
     loadOneTrustScript: function loadOneTrustScript(scriptUrl) {
-        console.log("Loading script", scriptUrl);
         if (this._loadingScript) return;
 
         this._loadingScript = true;
@@ -25,7 +24,7 @@ var VERSION_1 = {
                 // console.log('Loaded OneTrust script');
             },
             function() {
-                console.warn("Failed to load OneTrust script!"); // eslint-disable-line no-console
+                console.warn('Failed to load OneTrust script!'); // eslint-disable-line no-console
                 this._loadingScript = false;
             }.bind(this)
         );
@@ -47,96 +46,8 @@ var VERSION_1 = {
             this.consentedFunctionalCookies = /,3,/.test(this.consentData);
             this.consentedTargetingCookies = /,4,/.test(this.consentData);
         }
-        this.isDialogClosed = !!getCookie("OptanonAlertBoxClosed");
-    }
-};
-
-// looks like the new version is 5...
-var VERSION_2 = {
-    loadConsent: function loadConsent() {
-        var data = getCookie(this.ONETRUST_COOKIE) || "";
-        this.setConsent(data);
+        this.isDialogClosed = !!getCookie('OptanonAlertBoxClosed');
     },
-    loadOneTrustScript: function loadOneTrustScript(scriptUrl, domainScript) {
-        console.log("Loading script", scriptUrl, domainScript);
-        if (this._loadingScript) return;
-
-        this._loadingScript = true;
-
-        loadScript(scriptUrl, {
-            attributes: { "data-domain-script": domainScript }
-        }).then(
-            function() {
-                // console.log('Loaded OneTrust script');
-            },
-            function() {
-                console.warn("Failed to load OneTrust script!"); // eslint-disable-line no-console
-                this._loadingScript = false;
-            }.bind(this)
-        );
-    },
-    oneTrustCallback: function oneTrustCallback() {
-        this.setConsent(getCookie(ONETRUST_CONSENT_COOKIE));
-    },
-    processConsentData: function processConsentData() {
-        console.log("processing", this.consentData);
-
-        this.isDialogClosed = !!getCookie("OptanonAlertBoxClosed");
-
-        var cookieParts = this.consentData.split("&").filter(function(part) {
-            return /^groups=/i.test(part);
-        });
-        if (cookieParts.length === 0) return;
-
-        var groups = decodeURIComponent(cookieParts[0].split("=")[1]);
-        // these are always going to be true. you cannot turn these off.
-        this.consentedStrictlyCookies = true;
-        // this.consentedStrictlyCookies = /1:1/.test(groups);
-        this.consentedPerformanceCookies = /2:1/.test(groups);
-        this.consentedFunctionalCookies = /3:1/.test(groups);
-        this.consentedTargetingCookies = /4:1/.test(groups);
-        this.consentedSocialNetworkCookies = /5:1/.test(groups);
-    }
-};
-
-var VERSIONS = {
-    1: VERSION_1,
-    2: VERSION_2
-};
-
-function OneTrustManager() {
-    EventEmitter.call(this, {
-        wildcard: true
-    });
-    this.ONETRUST_COOKIE = "cnd_one_trust_consent2";
-    this.loadConsent = null;
-    this.loadOneTrustScript = null;
-    this.processConsentData = null;
-    this._loadingScript = false;
-    this.isDialogClosed = false;
-    this.consentData = "";
-    this.consentedStrictlyCookies = true;
-    this.consentedPerformanceCookies = false;
-    this.consentedFunctionalCookies = false;
-    this.consentedTargetingCookies = false;
-    this.consentedSocialNetworkCookies = false;
-}
-
-OneTrustManager.prototype = inherit(EventEmitter.prototype, {
-    constructor: OneTrustManager,
-
-    _emitChange: function _emitChange(data) {
-        // console.log('EVENT:CHANGE', data);
-        // console.table({
-        //     consentedStrictlyCookies: this.consentedStrictlyCookies,
-        //     consentedPerformanceCookies: this.consentedPerformanceCookies,
-        //     consentedFunctionalCookies: this.consentedFunctionalCookies,
-        //     consentedTargetingCookies: this.consentedTargetingCookies,
-        //     consentedSocialNetworkCookies: this.consentedSocialNetworkCookies,
-        // });
-        this.emit("change", createEventTemplate("change", this, data));
-    },
-
     setConsent: function setConsent(value) {
         var beforeValue = this.consentData;
 
@@ -149,11 +60,110 @@ OneTrustManager.prototype = inherit(EventEmitter.prototype, {
                 consentValue: this.consentData
             });
         }
+    }
+};
+
+// looks like the new version is 5...
+var VERSION_2 = {
+    loadConsent: function loadConsent() {
+        var data = getCookie(this.ONETRUST_COOKIE) || '';
+        this.setConsent(data);
+    },
+    loadOneTrustScript: function loadOneTrustScript(scriptUrl, domainScript) {
+        if (this._loadingScript) return;
+
+        this._loadingScript = true;
+
+        loadScript(scriptUrl, {
+            attributes: { 'data-domain-script': domainScript }
+        }).then(
+            function() {
+                // console.log('Loaded OneTrust script');
+            },
+            function() {
+                console.warn('Failed to load OneTrust script!'); // eslint-disable-line no-console
+                this._loadingScript = false;
+            }.bind(this)
+        );
+    },
+    oneTrustCallback: function oneTrustCallback() {
+        this.setConsent(getCookie(ONETRUST_CONSENT_COOKIE));
+    },
+    processConsentData: function processConsentData() {
+        this.isDialogClosed = !!getCookie('OptanonAlertBoxClosed');
+
+        var cookieParts = this.consentData.split('&').filter(function(part) {
+            return /^groups=/i.test(part);
+        });
+        if (cookieParts.length === 0) return;
+
+        var groups = decodeURIComponent(cookieParts[0].split('=')[1]);
+        // these are always going to be true. you cannot turn these off.
+        this.consentedStrictlyCookies = true;
+        // this.consentedStrictlyCookies = /1:1/.test(groups);
+        this.consentedPerformanceCookies = /2:1/.test(groups);
+        this.consentedFunctionalCookies = /3:1/.test(groups);
+        this.consentedTargetingCookies = /4:1/.test(groups);
+        this.consentedSocialNetworkCookies = /5:1/.test(groups);
+    },
+    setConsent: function setConsent(value) {
+        var beforeValue = this.consentData;
+
+        setCookie(this.ONETRUST_COOKIE, value);
+        this.consentData = value;
+        this.processConsentData.call(this);
+
+        if (beforeValue !== this.consent) {
+            this._emitChange({
+                consentValue: this.consentData
+            });
+        }
+
+        if (this.isDialogClosed) this._emitReady();
+    }
+};
+
+var VERSIONS = {
+    1: VERSION_1,
+    2: VERSION_2
+};
+
+function OneTrustManager() {
+    EventEmitter.call(this, {
+        wildcard: true
+    });
+    this.ONETRUST_COOKIE = 'cnd_one_trust_consent2';
+    this.loadConsent = null;
+    this.loadOneTrustScript = null;
+    this.processConsentData = null;
+    this.setConsent = null;
+    this._loadingScript = false;
+    this._isReady = false;
+    this.isDialogClosed = false;
+    this.consentData = '';
+    this.consentedStrictlyCookies = true;
+    this.consentedPerformanceCookies = false;
+    this.consentedFunctionalCookies = false;
+    this.consentedTargetingCookies = false;
+    this.consentedSocialNetworkCookies = false;
+}
+
+OneTrustManager.prototype = inherit(EventEmitter.prototype, {
+    constructor: OneTrustManager,
+
+    _emitChange: function _emitChange(data) {
+        this.emit('change', createEventTemplate('change', this, data));
+    },
+
+    _emitReady: function _emitReady(data) {
+        if (this._isReady) return;
+        this.emit('ready', createEventTemplate('ready', this, data));
+        this._isReady = true;
     },
 
     init: function init(options) {
-        if (!options.hasOwnProperty("script_url")) {
-            throw new Error("Missing script_url value");
+        if (!options.hasOwnProperty('script_url')) {
+            throw new Error('Missing script_url value');
         }
 
         // Default the version value to 1
@@ -166,12 +176,13 @@ OneTrustManager.prototype = inherit(EventEmitter.prototype, {
         this.processConsentData = VERSION_HANDLERS.processConsentData.bind(
             this
         );
+        this.setConsent = VERSION_HANDLERS.setConsent.bind(this);
         // Set the callback before trying to load the script
         window.OptanonWrapper = VERSION_HANDLERS.oneTrustCallback.bind(this);
 
         this.loadConsent();
         this.loadOneTrustScript(options.script_url, options.domain_script);
-        if (!hasCookiesEnabled) this.setConsent("");
+        if (!hasCookiesEnabled) this.setConsent('');
     }
 });
 
