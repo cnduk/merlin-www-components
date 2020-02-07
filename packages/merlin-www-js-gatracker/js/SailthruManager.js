1bcb1b9ae4a9532d5e24ec45d2afe6775e3c97f6
Rich Taylor <github@rsty.org>
2020-02-07 14:08:15 +0000

    feat(Sailthru): add initial sailthru stuff

---
+++ packages/merlin-www-js-gatracker/js/SailthruManager.js
@@ -0,0 +1,38 @@
+import EventEmitter from 'eventemitter2';
+import {
+    inherit,
+    loadScript
+} from '@cnbritain/merlin-www-js-utils/js/functions';
+
+function SailthruManager() {
+    EventEmitter.call(this, {
+        wildcard: true
+    });
+    this._sailthrewId = null;
+    this._hasLoadedScript = false;
+}
+
+SailthruManager.prototype = inherit(EventEmitter.prototype, {
+    constructor: SailthruManager,
+
+    init: function init(sailthrewId) {
+        this._sailthrewId = sailthrewId;
+    },
+
+    loadScript: function loadScript() {
+        if (this._hasLoadedScript) return;
+        if (this._sailthrewId === null) {
+            console.warn('Missing Sailthru Id', this._sailthrewId);
+            return;
+        }
+        this._hasLoadedScript = true;
+        loadScript('https://ak.sail-horizon.com/spm/spm.v1.min.js');
+        this.initSaleThrough();
+    },
+
+    initSaleThrough: function() {
+        Sailthru.init({customerId: this._sailthrewId});
+    }
+});
+
+export default new SailthruManager();
