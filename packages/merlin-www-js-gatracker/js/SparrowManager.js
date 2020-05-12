2b963e05c37143e2798d5c2b5c566c1b9da9fb75
samtiffin <sam@samtiffin.com>
2020-05-12 13:42:27 +0100

    feat(merlin-www-js-ga-tracker): Add SparrowManager for CN Sparrow/Pixel tracking

---
+++ packages/merlin-www-js-gatracker/js/SparrowManager.js
@@ -0,0 +1,39 @@
+import EventEmitter from 'eventemitter2';
+import {
+    inherit,
+    loadScript
+} from '@cnbritain/merlin-www-js-utils/js/functions';
+
+function SparrowManager() {
+    EventEmitter.call(this, {
+        wildcard: true
+    });
+    this._sparrowConfig = null;
+    this._hasLoadedScript = false;
+}
+
+SparrowManager.prototype = inherit(EventEmitter.prototype, {
+    constructor: SparrowManager,
+
+    init: function init(sparrowConfig) {
+        this._sparrowConfig = sparrowConfig;
+    },
+
+    loadScript: function loadScript() {
+        if (this._hasLoadedScript) return;
+        if (this._sparrowConfig === null) {
+            console.warn('Missing Sparrow Config', this._sparrowConfig);
+        }
+        
+        this._hasLoadedScript = true;
+
+        loadScript('https://pixel.condenastdigital.com/config/' + this.sparrowConfig + '.config.js').then(
+            function() {
+                loadScript('https://pixel.condenastdigital.com/sparrow.min.js');
+            },
+            function() {
+                console.warn('Failed loading Sparrow Config!');
+            }
+        );
+    }
+});
