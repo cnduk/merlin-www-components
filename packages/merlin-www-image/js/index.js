a1539a9134d9c9fe12f69c72de19b80d7eca6f8c
samtiffin <sam@samtiffin.com>
2020-08-26 17:53:22 +0100

    UPDATE: Set lazysizes config directly

--- packages/merlin-www-image/js/index.js
+++ packages/merlin-www-image/js/index.js
@@ -4,30 +4,28 @@
  * @module image
  */
 
-window.lazySizesConfig = window.lazySizesConfig || {};
-// Make sure we dont auto trigger lazySizes instantly
-window.lazySizesConfig.init = false;
-// Marker for the lazyload image
-window.lazySizesConfig.lazyClass = 'c-image--lazyload';
-// Marker for the images to be preloaded after load
-window.lazySizesConfig.preloadClass = 'c-image--lazypreload';
-// Class to be applied when loading
-window.lazySizesConfig.loadingClass = 'c-image--lazyload-loading';
-// Class to be applied when loaded
-window.lazySizesConfig.loadedClass = 'c-image--lazyload-loaded';
-
 import lazySizes from 'lazysizes';
 
 //IE Polyfill for picture element
 import 'lazysizes/plugins/respimg/ls.respimg.js';
 
-export default {
+lazySizes.cfg = window.lazySizesConfig || {};
+// Make sure we dont auto trigger lazySizes instantly
+lazySizes.cfg.init = false;
+// Marker for the lazyload image
+lazySizes.cfg.lazyClass = 'c-image--lazyload';
+// Marker for the images to be preloaded after load
+lazySizes.cfg.preloadClass = 'c-image--lazypreload';
+// Class to be applied when loading
+lazySizes.cfg.loadingClass = 'c-image--lazyload-loading';
+// Class to be applied when loaded
+lazySizes.cfg.loadedClass = 'c-image--lazyload-loaded';
 
+export default {
     /**
      * Initialises the loading of images
      */
-    'init': function() {
+    'init': function () {
         lazySizes.init();
     }
-
 };
